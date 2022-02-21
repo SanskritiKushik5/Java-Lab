390bab5e019b62c3be8d9b61eec460f894ba2d88
Sanskriti Kushik <sanskritikushik23@gmail.com>
2022-02-21 13:28:31 +0530

    Add files via upload

---
+++ Lab-1/MyFirstProgram2.java
@@ -0,0 +1,12 @@
+class MyFirstProgram2
+{
+   void display()
+   {
+      System.out.println("Hello, I am enjoying Java");
+   }
+   public static void main(String arg[])
+   {
+      MyFirstProgram2 obj = new MyFirstProgram2();
+      obj.display();
+   }
+}
