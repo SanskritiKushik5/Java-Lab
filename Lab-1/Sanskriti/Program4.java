4c6352c1711d9d6dd8aaf2395b27a81296aa0980
Sanskriti Kushik <sanskritikushik23@gmail.com>
2022-02-21 14:15:35 +0530

    Add files via upload

---
+++ Lab-1/Sanskriti/Program4.java
@@ -0,0 +1,11 @@
+import UMIT.Calculation;
+
+class Program4
+{
+   public static void main(String arg[])
+   {
+      Calculation obj = new Calculation();
+      System.out.println(obj.add(4, 5));
+      System.out.println(obj.subtract(5, 4));
+   }
+}
